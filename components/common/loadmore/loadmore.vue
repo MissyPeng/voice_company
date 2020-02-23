c3d42b4e2000efd49116df3308df77d4f37939e3
Nancy <1552320235@qq.com>
2020-02-23 12:57:23 +0800

    加载更多组件

---
+++ components/common/loadmore/loadmore.vue
@@ -0,0 +1,28 @@
+<template>
+	<view>
+		<view class="loadmore">{{loadtest}}</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				props:{
+					loadtest: String
+				}
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style scoped>
+.loadmore{
+	text-align: center;
+	color: #AAAAAA;
+	padding: 15upx;
+}
+</style>
