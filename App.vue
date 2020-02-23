b043712580cbaf6fddda10bdeda27cacdbaac622
Nancy <1552320235@qq.com>
2020-02-23 13:16:47 +0800

    应用配置，用来配置App全局样式以及监听

---
+++ App.vue
@@ -0,0 +1,28 @@
+<template>
+	<view>
+		
+	</view>
+</template>
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+	/* 引入官方css库 */
+	@import './common/uni.css';
+	/* 引入自定义css库 */
+	@import './common/icon.css';
+	/* 引入动画库 */
+	@import './common/animate.css';
+</style>
